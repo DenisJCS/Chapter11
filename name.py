1fc24c794c14dd778e1f49fcf9f523cce34e11c7
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-09-15 22:00:19 -0400

    Update name.py

--- name.py
+++ name.py
@@ -1,1 +1,15 @@
+from names_function import get_formatted_name
 
+print("Enter 'q' to quit program.")
+
+while True:
+    first = input("\nPlease enter your frist name: ")
+    if first == 'q':
+        break
+    last = input("Please enter you last name:")
+    if last == 'q':
+        break
+
+    formatted_name = get_formatted_name(first, last)
+    print(f"\tNeatly formatted name: {formatted_name}")
+    
