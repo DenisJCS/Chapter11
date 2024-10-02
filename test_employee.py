50dde2d1324fe5f2f0aede9dace1ca1e905b9af0
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-10-02 14:56:45 -0400

    Update test_employee.py

--- test_employee.py
+++ test_employee.py
@@ -1,1 +1,13 @@
+from employee import Employee
+"""Test employee program """
 
+def test_give_default():
+    employee = Employee('Chandler', 'Bing', 20000)
+    employee.give_raise()
+    assert employee.annual_salary == 25000 
+
+def test_give_custom_raise():
+    employee = Employee('Jouye', 'Tribiani', 23000)
+    employee.give_raise(10000)
+    assert employee.annual_salary == 33000
+    
