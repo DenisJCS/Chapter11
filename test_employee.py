58fd91a670b6829ba294181c8cde5b943d75aac3
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-10-02 14:57:21 -0400

    Update test_employee.py

--- test_employee.py
+++ test_employee.py
@@ -11,3 +11,21 @@ def test_give_custom_raise():
     employee.give_raise(10000)
     assert employee.annual_salary == 33000
     
+python3 -m pytest
+========================================== test session starts ===========================================
+platform darwin -- Python 3.12.3, pytest-8.3.3, pluggy-1.5.0
+rootdir: /Users/denis_jcs/Documents/Chapter11/employee
+collected 1 item                                                                                         
+
+test_employee.py .                                                                                 [100%]
+
+=========================================== 1 passed in 0.00s ============================================
+python3 -m pytest 
+========================================== test session starts ===========================================
+platform darwin -- Python 3.12.3, pytest-8.3.3, pluggy-1.5.0
+rootdir: /Users/denis_jcs/Documents/Chapter11/employee
+collected 2 items                                                                                        
+
+test_employee.py ..                                                                                [100%]
+
+=========================================== 2 passed in 0.01s ============================================
