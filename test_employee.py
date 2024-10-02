87c13f2c09123efd1b7c74750991a5f9e52fe0cb
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-10-02 15:06:11 -0400

    Update test_employee.py

--- test_employee.py
+++ test_employee.py
@@ -29,3 +29,36 @@ collected 2 items
 test_employee.py ..                                                                                [100%]
 
 =========================================== 2 passed in 0.01s ============================================
+
+
+
+import pytest
+from employee import Employee
+"""Test employee program """
+
+@pytest.fixture
+def employee():
+    return Employee("Ross", "Geller", 75000)
+
+def test_give_default(employee):
+    employee.give_raise()
+    assert employee.annual_salary == 80000 
+
+def test_give_custom_raise(employee):
+    employee.give_raise(10000)
+    assert employee.annual_salary == 85000
+
+python3 -m pytest
+============================================ test session starts =============================================
+platform darwin -- Python 3.12.3, pytest-8.3.3, pluggy-1.5.0
+rootdir: /Users/denis_jcs/Documents/Chapter11/employee
+collected 2 items                                                                                            
+
+test_employee.py ..                                                                                    [100%]
+
+============================================= 2 passed in 0.00s ==============================================
+
+
+
+
+
