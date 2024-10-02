878d6d1b7f23e3dfb713621b473dd784a4f4e9d1
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-10-02 14:55:40 -0400

    Update employee.py

--- employee.py
+++ employee.py
@@ -1,1 +1,11 @@
+class Employee:
+    """Collect name and last name and give annual salary."""
+    def __init__(self, first_name, last_name, annual_salary):
+        """Collect data of full name and annual salary"""
+        self.first = first_name
+        self.last = last_name
+        self.annual_salary = annual_salary
+    def give_raise(self,amount = 5000 ):
+        """Give raise to salary"""
+        self.annual_salary += amount
 
